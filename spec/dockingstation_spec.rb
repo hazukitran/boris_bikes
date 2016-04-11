a114edb79380020a8120f89d63459e2317ec6da6
Ben Stein <bumstain@gmail.com>
2016-04-12 01:57:15 +1000

    Docking stations get bikes

--- spec/dockingstation_spec.rb
+++ spec/dockingstation_spec.rb
@@ -2,4 +2,8 @@ require 'dockingstation'
 
 describe DockingStation do  #subject { DockingStation.new }
   it { is_expected.to respond_to :release_bike }
+  it 'gets bike' do
+    bike = subject.release_bike
+    expect(bike).to be_working
+  end
 end
