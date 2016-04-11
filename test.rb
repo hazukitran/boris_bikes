3ac1eba16e3be64df8be75a2b48316a6d67b4231
Ben Stein <bumstain@gmail.com>
2016-04-12 00:29:09 +1000

    Initialized RSpec

---
+++ test.rb
@@ -0,0 +1,8 @@
+docking_station = DockingStation.new 
+
+# test.rb:1:in `<main>': uninitialized constant DockingStation (NameError)
+# NameError
+# Users/benstein/Documents/Git/makers_academy/test.rb
+# Line 1
+# Raised when a given name is invalid or undefined. raises the exception: NameError
+# Initialize a DockingStation class 
